35c686e208cf94fe5a3976e4caf6f10884114b75
Rebecca <techybecky21@gmail.com>
2023-04-24 20:23:49 +0100

    increment works

--- main.js
+++ main.js
@@ -7,19 +7,4 @@ let count = 0;
 counterPlus = addEventListener('click', function(){
     count++;
     output.textContent = count;
-    if (count > 0){
-        output.style.color = "green";
-    }
-})  
-counterMinus = addEventListener('click', function(){
-    count--;
-    output.textContent = count;
-    if (count < 0){
-        output.style.color = "red";
-    }
 })
-reset = addEventListener('click', function(){
-    output.textContent = 0;
-    output.style.color = "black";
-})
-
